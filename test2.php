f28dabf3d2d35f898e3a7c1957daa611cd439826
jacobsonsballoon <p_dang123@hotmail.com>
2020-11-11 15:43:18 +0700

    Update test2.php

--- test2.php
+++ test2.php
@@ -1,19 +1,12 @@
 <?php
 
-//header('Content-Type: charset=utf-8');
-//header('Content-type:application/json;charset=utf-8');
+error_reporting(E_ALL);
+
 $json = file_get_contents('http://49.231.247.41/LineBot/line1.php');
     $obj = json_decode($json);
     $fname= $obj[0]->fname;
 
-//echo "fname1 = ".tis_utf8($fname)."<br/>";
-
-
-//echo "OK";
-
  echo "fname1 === ".$fname." === Tกกกก";
-
-
 
 
 

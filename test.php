e3771b0f8a727b33b7797681464297ca3bf9d754
jacobsonsballoon <p_dang123@hotmail.com>
2020-11-11 14:38:12 +0700

    Update test.php

--- test.php
+++ test.php
@@ -17,7 +17,7 @@ $json = file_get_contents('http://49.231.247.41/LineBot/line1.php');
 $obj = json_decode($json);
 $fname= $obj[0]->fname;
 
-echo "fname1 = ".$fname;
+echo "fname1 = ".tis_utf8($fname);
 
 if ( sizeof($request_array['events']) > 0 ) {
 

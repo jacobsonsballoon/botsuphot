3712925bc818601dc54a7673e3569b60f9986426
jacobsonsballoon <p_dang123@hotmail.com>
2020-11-11 14:27:27 +0700

    Update test.php

--- test.php
+++ test.php
@@ -26,7 +26,7 @@ if ( sizeof($request_array['events']) > 0 ) {
        $reply_message = '';
         $reply_token = $event['replyToken'];
         $text = $event['message']['text'];
-        $fname= $obj[0]->fname;
+        $fname= tis_utf8($obj[0]->fname);
         $data = [
             'replyToken' => $reply_token,
             'messages' => [['type' => 'text', 'text' => $fname ]]
@@ -57,5 +57,44 @@ function send_reply_message($url, $post_header, $post_body)
 
     return $result;
 }
+
+function tis_utf8($tis)
+{
+    $max = strlen($tis);
+    $utf8 = "";
+    for ($i = 0; $i < $max; $i++) {
+        $s = substr($tis, $i, 1);
+        $val = ord($s);
+        if ($val < 0x80) {
+            $utf8 .= $s;
+        } elseif ((0xA1 <= $val and $val <= 0xDA) or (0xDF <= $val and $val <= 0xFB)) {
+            $unicode = 0x0E00 + $val - 0xA0;
+            $utf8 .= chr(0xE0 | ($unicode >> 12));
+            $utf8 .= chr(0x80 | (($unicode >> 6) & 0x3F));
+            $utf8 .= chr(0x80 | ($unicode & 0x3F));
+        }
+    }
+    return $utf8;
+}
+
+function utf8_tis($string)
+{
+    $str = $string;
+    $res = "";
+    for ($i = 0; $i < strlen($str); $i++) {
+        if (ord($str[$i]) == 224) {
+            $unicode = ord($str[$i + 2]) & 0x3F;
+            $unicode |= (ord($str[$i + 1]) & 0x3F) << 6;
+            $unicode |= (ord($str[$i]) & 0x0F) << 12;
+            $res .= chr($unicode - 0x0E00 + 0xA0);
+            $i += 2;
+        } else {
+            $res .= $str[$i];
+        }
+    }
+    return $res;
+}
+
+
 
 ?>

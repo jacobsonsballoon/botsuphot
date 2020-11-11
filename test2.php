67b9e9ac4f0b5d0ec5f708060687d3a783b83fec
jacobsonsballoon <p_dang123@hotmail.com>
2020-11-11 15:56:09 +0700

    Update test2.php

--- test2.php
+++ test2.php
@@ -2,12 +2,12 @@
 
 error_reporting(E_ALL);
 
-//$json = file_get_contents('http://49.231.247.41/LineBot/line1.php');
-$json = '[{"fname":"SUPHOT","lname":"SUPHOT"}]';
+$json = file_get_contents('http://49.231.247.41/LineBot/line1.php');
+//$json = '[{"fname":"SUPHOT","lname":"SUPHOT"}]';
     $obj = json_decode($json);
     $fname= $obj[0]->fname;
 
- echo "fname1 === ".$fname." === Tกกกก";
+ echo "fname1 === ".$json." === Tกกกก";
 
 
 

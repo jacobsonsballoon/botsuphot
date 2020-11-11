f84cc7c51f9b0b80be965b04b127a52665b8153d
jacobsonsballoon <p_dang123@hotmail.com>
2020-11-11 15:23:01 +0700

    Update test2.php

--- test2.php
+++ test2.php
@@ -6,7 +6,7 @@ $json = file_get_contents('http://49.231.247.41/LineBot/line1.php');
     $obj = json_decode($json);
     $fname= $obj[0]->fname;
 
-echo "fname1 = ".tis_utf8($fname)."<br/>";
+//echo "fname1 = ".tis_utf8($fname)."<br/>";
 
 
 //echo "OK";

861d7e54c9b397749cad5f497fa90cf19831da06
jacobsonsballoon <p_dang123@hotmail.com>
2020-11-11 15:21:51 +0700

    Update test2.php

--- test2.php
+++ test2.php
@@ -1,7 +1,7 @@
 <?php
 
 //header('Content-Type: charset=utf-8');
-
+header('Content-type:application/json;charset=utf-8');
 $json = file_get_contents('http://49.231.247.41/LineBot/line1.php');
     $obj = json_decode($json);
     $fname= $obj[0]->fname;

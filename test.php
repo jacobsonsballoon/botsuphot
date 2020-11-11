43a0ed8c44e20b9847381616b75e80bb45abba50
jacobsonsballoon <p_dang123@hotmail.com>
2020-11-11 14:20:15 +0700

    Update test.php

--- test.php
+++ test.php
@@ -28,7 +28,7 @@ if ( sizeof($request_array['events']) > 0 ) {
         $text = $event['message']['text'];
         $data = [
             'replyToken' => $reply_token,
-            'messages' => [['type' => 'text', 'text' => 'สุพจน์' ]]
+            'messages' => [['type' => 'text', 'text' => $fname ]]
         ];
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
         $send_result = send_reply_message($API_URL.'/reply',      $POST_HEADER, $post_body);

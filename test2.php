095a9ff0a6316f9d8084f0453f6e9bd0e9ee71f0
jacobsonsballoon <p_dang123@hotmail.com>
2020-11-12 10:23:08 +0700

    Update test2.php

--- test2.php
+++ test2.php
@@ -1,28 +1,15 @@
 <?php
 
 error_reporting(E_ALL);
-$url = 'http://49.231.247.41/LineBot/line1.php';
-$opts = array('http' => array(
-    'method' => "GET",
-    'header' => "Accept-language: en\r\n" . "Cookie: ".session_name()."=".session_id()."\r\n" ) 
-);
-$context = stream_context_create($opts);
-session_write_close();   // this is the key
-$html = file_get_contents($url, false, $context);       // <-- line 92  
-echo $html;
+//$url = 'http://49.231.247.41/LineBot/line1.php';
 
-echo phpinfo();
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "http://49.231.247.41/LineBot/line1.php"); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$temp = trim(curl_exec($ch));
+curl_close($ch);
 
-
-
-
-
-//$json = file_get_contents('http://49.231.247.41/LineBot/line1.php');
-//$json = '[{"fname":"SUPHOT","lname":"SUPHOT"}]';
-    //$obj = json_decode($json);
-    //$fname= $obj[0]->fname;
-
- //echo "fname1 === ".$json." === Tกกกก";
+echo "temp == ".$temp;
 
 
 
